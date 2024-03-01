7d7e0851a7b0120e7db08b0d2feecc8cc68186e5
Payal <payalbhourakatoch@gmail.com>
2024-03-01 22:31:34 +0530

    EcommerceJS

---
+++ public/home.js
@@ -0,0 +1,10 @@
+var desbtn=document.getElementById("desbtn");
+var cartbtn = document.getElementById("cart");
+
+cartbtn.addEventListener("click",function(){
+
+})
+desbtn.addEventListener("click",fucntion(){
+     
+})
+   
